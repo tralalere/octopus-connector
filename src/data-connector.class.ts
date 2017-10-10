1287975f7cf96d815d9f0c30911bd9c2835c5cf5
Christophe Vimbert <c.vimbert@outlook.fr>
2017-10-10 16:48:25 +0200

    Project base

---
+++ src/data-connector.class.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by Christophe on 10/10/2017.
+ */
+import {DataConnectorConfig} from "./data-connector-config.interface";
+
+export class DataConnector {
+
+    constructor(configuration:DataConnectorConfig) {
+
+    }
+}
---
+++ src/data-interfaces/external-interface.interface.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by Christophe on 10/10/2017.
+ */
+export interface ExternalInterface {
+    
+}
