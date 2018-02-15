b20929426b8c59002ee5aedee3952f20533b05ff
Christophe <christophe.vimbert@tralalere.com>
2018-02-15 11:43:15 +0100

    Adding webpack for build

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var webpack = require("webpack");
+
+module.exports = {
+    entry: './index.ts',
+    output: {
+        filename: "./dist/octopus-connect.umd.js",
+        libraryTarget: 'umd',
+        library: "OctopusConnect"
+    },
+    resolve: {
+        extensions: [".webpack.js", ".web.js", ".js", ".ts"],
+        alias: {
+
+        }
+    },
+    module: {
+        loaders: [
+            { test: /\.ts?$/, loader: "ts-loader" }
+        ]
+    },
+    externals: /(rxjs)/i
+};
